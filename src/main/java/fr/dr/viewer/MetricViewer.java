82d794052fedd0decfea2bff3ee7bdfc0a6a713c
drieu <damien.rieu@gmail.com>
2013-03-07 23:56:36 +0100

    Begin FXML project

---
+++ src/main/java/fr/dr/viewer/MetricViewer.java
@@ -0,0 +1,31 @@
+package fr.dr.viewer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * .
+ * User: drieu
+ * Date: 3/7/13
+ * Time: 10:37 PM
+ */
+public class MetricViewer extends
+        Application {
+
+
+    public static void main(String[] args) {
+        MetricViewer.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fr/dr/viewer/metric_viewer.fxml"));
+
+        stage.setTitle("FXML Welcome");
+        stage.setScene(new Scene(root, 300, 275));
+        stage.show();
+    }
+}
---
+++ src/main/java/fr/dr/viewer/MetricViewerController.java
@@ -0,0 +1,16 @@
+package fr.dr.viewer;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+/**
+ * .
+ * User: drieu
+ * Date: 3/7/13
+ * Time: 10:50 PM
+ */
+public class MetricViewerController {
+
+    @FXML
+    private Text info;
+}
